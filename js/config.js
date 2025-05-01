8d0634b0147a49c7137c8a376e80341f7ba240f2
Force21MB <blauertz.marco@web.de>
2025-05-01 19:57:07 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@
  * Konfigurationsdatei für das VIS-System
  */
 
-// Konfiguration für die Verbindung zur API
 const CONFIG = {
     SUPABASE_URL: 'https://hqgmepblrpjvqctegfbj.supabase.co',
     SUPABASE_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZ21lcGJscnBqdnFjdGVnZmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTY2ODYsImV4cCI6MjA2MTY3MjY4Nn0.qY2-fJHgAl_xtDvTdaODrwT74lkW8cRybEwQIi6nLEM',
@@ -24,10 +23,10 @@ const CONFIG = {
         bezirk: 'bezirk',
         aufgrabung_eingereicht: 'aufgrabung_eingereicht',
         zeitraum_von: 'zeitraum_von',
-        zeitraum_bis: 'zeitraum-bis',
+        zeitraum_bis: 'zeitraum_bis',
         vis_Nr: 'vis_Nr',
         tk_Nr: 'tk_Nr',
-        datum_tiefbau_erledigt: 'datum_tiefbau-erledigt',
+        datum_tiefbau_erledigt: 'datum_tiefbau_erledigt',
         tiefbau_firma: 'tiefbau_firma',
         mitarbeiter: 'mitarbeiter',
         bemerkung: 'bemerkung'
