deaa581012aa7b42b4538cd7fead061e5fdc51d8
Force21MB <blauertz.marco@web.de>
2025-05-01 19:50:49 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,17 @@ const CONFIG = {
     SUPABASE_URL: 'https://hqgmepblrpjvqctegfbj.supabase.co',
     SUPABASE_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZ21lcGJscnBqdnFjdGVnZmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTY2ODYsImV4cCI6MjA2MTY3MjY4Nn0.qY2-fJHgAl_xtDvTdaODrwT74lkW8cRybEwQIi6nLEM',
     TABLE_NAME: 'vis_liste',
-    COLOGNE_DISTRICTS: ['Innenstadt', 'Rodenkirchen', 'Lindenthal', 'Ehrenfeld', 'Nippes', 'Chorweiler', 'Porz', 'Kalk', 'Mülheim'],
+    COLOGNE_DISTRICTS: [
+        'Innenstadt',
+        'Rodenkirchen',
+        'Lindenthal',
+        'Ehrenfeld',
+        'Nippes',
+        'Chorweiler',
+        'Porz',
+        'Kalk',
+        'Mülheim'
+    ]
 };
     
     // Tabellenspalten-Mapping
